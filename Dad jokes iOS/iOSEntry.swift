33061a8e6b4a6187dc495f2f98ec30901bd5bfea
Balsa Komnenovic <komnenovicbalsa93@gmail.com>
2023-03-09 23:04:45 +0100

    Adapted to iPad and iPhone

---
+++ Dad jokes iOS/iOSEntry.swift
@@ -0,0 +1,17 @@
+//
+//  Dad_jokes_iOSApp.swift
+//  Dad jokes iOS
+//
+//  Created by Balsa Komnenovic on 9.3.23..
+//
+
+import SwiftUI
+
+@main
+struct Dad_jokes_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            IOSJokeView()
+        }
+    }
+}
--- Shared/JokeView.swift
+++ Shared/JokeView.swift
@@ -12,12 +12,37 @@ struct JokeView: View {
     @State private var fetching = false
     @AppStorage("jokeType") var jokeType: JokeType = .dadJoke
     
+    var imageWidth: CGFloat {
+        #if os(macOS)
+        return 60
+        #else
+        return 120
+        #endif
+    }
+    
+    var font: Font {
+        #if os(macOS)
+        return .body
+        #else
+        return .title
+        #endif
+    }
+    
+    var frameWidth: CGFloat {
+        #if os(macOS)
+        return 205
+        #else
+        return min(UIScreen.main.bounds.size.width - 20, 600)
+        #endif
+    }
+    
     var body: some View {
         VStack {
             HStack(alignment: .center) {
                 Image("dad")
                     .resizable()
-                    .frame(width: 60, height: 60)
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: imageWidth)
                     .cornerRadius(8)
                 VStack {
                     ForEach(JokeType.allCases, id: \.self) { item in
@@ -30,27 +55,41 @@ struct JokeView: View {
                             Text(item.rawValue)
                                 .foregroundColor(item == jokeType ? .red : Color.primary)
                         }
+                        #if os(macOS)
+                        .cornerRadius(6)
+                        #else
+                        .buttonStyle(.bordered)
+                        .background(Color(.systemGray5))
+                        .cornerRadius(12)
+                        #endif
                     }
                 }
-                .frame(height: 130)
             }
             if fetching {
                 ProgressView()
             } else {
                 VStack {
                     Text(jokeString)
+                        .font(font)
                         .minimumScaleFactor(0.5)
                     HStack {
                         Spacer()
                         Button {
+                            #if os(macOS)
                             let pasteBoard = NSPasteboard.general
                             pasteBoard.clearContents()
                             pasteBoard.setString(jokeString, forType: NSPasteboard.PasteboardType.string)
+                            #else
+                            let pasteBoard = UIPasteboard.general
+                            pasteBoard.string = jokeString
+                            #endif
                         } label: {
                             Text("Copy")
                         }
+                        .buttonStyle(.borderedProminent)
                     }
                 }
+                .frame(width: frameWidth)
             }
             Spacer()
         }
@@ -78,7 +117,11 @@ struct JokeView: View {
 
 struct JokeView_Previews: PreviewProvider {
     static var previews: some View {
+        #if os(iOS)
+        JokeView()
+        #else
         JokeView()
             .frame(width: 225, height: 255)
+        #endif
     }
 }
